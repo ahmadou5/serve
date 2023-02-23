08949fdf901b655c386c28de3d0bb499fb18bfb3
ahmadou <ahmadlasauwal@gmail.com>
2023-02-23 13:28:17 +0100

    '===3'

--- src/server.js
+++ src/server.js
@@ -42,7 +42,7 @@ connection.connect((err) => {
       console.log(`connected to Mysql Server ${connection.threadId}`)
    }
 })*/}
-const PORT = 3002;
+
 
 app.use('/api/auth',authRoute);
 app.use('/api/profile', profileRoute)
@@ -55,7 +55,7 @@ app.use('/api/search', searchRoute)
 app.use('/api/email', mailRoute)
 app.use('/api/chat', chatRoute)
 
-app.listen(PORT, () => console.log(`Server is Listening to ${PORT}`));
+app.listen(3000, () => console.log(`Server is Listening to ${3000}`));
 app.get('/', (req,res) => {
    res.send(users)
 })
